5df8ec4e4d43a6782fb8ebba47801696eff88908
csh4017 <csh4017@ic.ac.uk>
2019-02-05 21:21:48 +0000

    Merge branch 'sike, this aint a conflict resolution' of https://gitlab.doc.ic.ac.uk/lab1819_spring/wacc_06

--- preflight.js
+++ preflight.js
@@ -1,7 +1,8 @@
 const fs = require('fs')
 const commitmessage = fs.readFileSync(process.env.HUSKY_GIT_PARAMS, 'utf8')
 const commitRegex = /^<((csh4017)|(pt3717)|(hl5617)|(st5317))> [A-Za-z0-9!"#$%&'()*+,.\r\n\/:;<=>?@\[\] ^_`{|}~-]+$/
-if (commitRegex.test(commitmessage)) {
+const mergeRegex = /Merge branch '.+' of https:\/\/gitlab.doc.ic.ac.uk\/lab1819_spring\/wacc_06(\n\r)?/
+if (commitRegex.test(commitmessage) || mergeRegex.test(commitmessage)) {
   console.log('  \x1b[32m%s\x1b[0m %s', '✔', 'Commit Message is OK')
   process.exit(0)
 } else {
