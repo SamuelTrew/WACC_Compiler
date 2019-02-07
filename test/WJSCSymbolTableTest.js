41455a8cb17d1046dc0f19fa6c9becee45549e30
Hao  Liang <hao.liang17@imperial.ac.uk>
2019-02-07 12:04:49 +0000

    <hl5617> Added Symbol Table tests

--- test/WJSCSymbolTableTest.js
+++ test/WJSCSymbolTableTest.js
@@ -1,11 +1,16 @@
 const { WJSCSymbolTable } = require('../build/WJSCSymbolTable')
 const assert = require('assert')
 
-// describe('Scope', function () {
-//     const symbolTable = new WJSCSymbolTable(0, undefined)
-//     describe('Enter Scope', function () {
-//         it('should increment scope number', function () {
-//             assert()
-//         })
-//     })
-// })
+describe('Scope', function () {
+    let symbolTable = new WJSCSymbolTable(0, undefined)
+    describe('Enter Scope', function () {
+        let childTable = symbolTable.enterScope()
+        it('should increment scope number', function () {
+            assert(childTable.getScopeLevel() === 1)
+        })
+        it('should have set parent level', function () {
+            assert(childTable.getParentTable() !== undefined)
+            assert(childTable.getParentTable() === symbolTable)
+        })
+    })
+})
